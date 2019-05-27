bd7120a4df6cd967d3b7f3374a4b7f19d395149b
Eric J. Van der Velden <eric.vander.velden@sogeti.com>
2019-05-27 22:46:21 +0200

    first commit

---
+++ call_psql.bat
@@ -0,0 +1,3 @@
+@echo off
+set DIR=C:\"Program Files"\PostgreSQL\9.6\bin
+%DIR%\psql -Ueric test
---
+++ h2_client_with_args1.bat
@@ -0,0 +1,3 @@
+@echo off
+set DIR=C:\Users\ervelden\bin\wildfly-10.1.0.Final
+java -classpath %DIR%\modules\system\layers\base\com\h2database\h2\main\h2-1.3.173.jar org.h2.tools.Shell -url jdbc:h2:tcp://localhost/h2/test -user sa -password sa
